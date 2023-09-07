d4066c5381a4431e2d526655d89b6a1faef3a824
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-07 14:58:56 -0400

    Update load_board.js

--- website/moon/js/load_board.js
+++ website/moon/js/load_board.js
@@ -23,9 +23,9 @@ function clear_board() {
     var selected_year = document.querySelector('input[name="year"]:checked').value;
     var background = document.querySelector('[title="MoonBoard Wall"]')
     if (selected_year == "2020") {
-        background.style.backgroundImage = 'url("../img/minimoonboard.png")';
+        background.style.backgroundImage = 'url("moon/img/minimoonboard.png")';
     } else {
-        background.style.backgroundImage = 'url("../img/moonboard.png")';
+        background.style.backgroundImage = 'url("moon/img/moonboard.png")';
     }
 }
 
