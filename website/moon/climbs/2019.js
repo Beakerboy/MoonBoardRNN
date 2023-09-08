eedf17c76d832868f93497bc73d287555edf0eab
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-08 08:10:47 -0400

    Update 2019.js

--- website/moon/climbs/2019.js
+++ website/moon/climbs/2019.js
@@ -1,1 +1,1 @@
-var climbs_2019 = {}
+var climbs_2019 = {"original": [{"moves": ["H6", "I5", "J4", "F8", "E11", "C11", "A8", "C14", "F16", "F18"], "grade": {"original": "V4", "XGBoost": "V4", "Keras MLP": "V4", "Random Forest": "V3", "Keras LSTM": "V3"}}]}
